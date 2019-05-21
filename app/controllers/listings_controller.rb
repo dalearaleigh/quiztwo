e217440805a6bf555a43fd1c057feaec927c1817
Raleigh Dale <dalearaleigh@gmail.com>
2019-05-21 16:29:00 +0000

    handle form submission

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,4 +8,15 @@ class ListingsController < ApplicationController
     @listing = Listing.new
   end 
 
+  def create 
+    Listing.create(listing_params)
+    redirect_to root_path 
+  end 
+
+  private 
+
+  def listing_params
+    params.require(:address).permit(:description, :price)
+  end 
+
 end
